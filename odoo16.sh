497adb97a0d10de6fc39563e1816c8cd3aa99b6a
Fernando H Vinha <41375095+FernandoVinha@users.noreply.github.com>
2023-10-06 07:57:14 -0300

    Update odoo16.sh
    Update

--- odoo16.sh
+++ odoo16.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-# Check if Docker is installed
+# Solicita o nome do projeto ao usuário
+read -p "Enter the project name: " PROJECT_NAME
+
+# Define o diretório do script e o caminho para os módulos adicionais
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ADDITIONAL_MODULES_PATH="${SCRIPT_DIR}/${PROJECT_NAME}/addons"
+
+# Cria o diretório para módulos adicionais se ele não existir
+mkdir -p "${ADDITIONAL_MODULES_PATH}"
+
+# Verifica e instala o Docker se necessário
 if ! command -v docker &> /dev/null
 then
     echo "Docker not found. Installing..."
@@ -12,11 +22,10 @@ else
     echo "Docker is already installed."
 fi
 
-# Check if Docker Compose is installed
+# Verifica e instala o Docker Compose se necessário
 if ! command -v docker-compose &> /dev/null
 then
     echo "Docker Compose not found. Installing..."
-    sudo apt update
     sudo apt install -y curl
     sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
@@ -24,39 +33,33 @@ else
     echo "Docker Compose is already installed."
 fi
 
-# Display the installed Docker Compose version
+# Exibe a versão do Docker Compose instalada
 docker-compose --version
 
-# Get the directory where the script is being executed
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-# Define the path for additional modules
-ADDITIONAL_MODULES_PATH="${SCRIPT_DIR}/addons"
-
-# Create the addons directory if it does not exist
-mkdir -p "${ADDITIONAL_MODULES_PATH}"
-
-# Pull Docker images
+# Baixa as imagens Docker necessárias
 echo "Pulling the Odoo 16.0 image..."
 docker pull odoo:16.0
 
 echo "Pulling the PostgreSQL 13 image..."
 docker pull postgres:13
 
-# Start the PostgreSQL container
-echo "Starting the PostgreSQL container..."
-docker run -d \
-    -e POSTGRES_USER=odoo \
-    -e POSTGRES_PASSWORD=odoo \
-    -e POSTGRES_DB=postgres \
-    --name meu_db \
-    postgres:13
+# Verifica se já existe um container PostgreSQL rodando com o nome do projeto
+if [ "$(docker ps -q -f name=${PROJECT_NAME}_db)" ]; then
+    echo "A PostgreSQL container named ${PROJECT_NAME}_db is already running. Skipping the database setup..."
+else
+    echo "Starting the PostgreSQL container named ${PROJECT_NAME}_db..."
+    docker run -d \
+        -e POSTGRES_USER=odoo \
+        -e POSTGRES_PASSWORD=odoo \
+        -e POSTGRES_DB=postgres \
+        --name ${PROJECT_NAME}_db \
+        postgres:13
+    
+    echo "Waiting for PostgreSQL to initialize..."
+    sleep 10
+fi
 
-# Wait a few seconds to ensure PostgreSQL is ready
-echo "Waiting for PostgreSQL to initialize..."
-sleep 10
-
-# Check if netstat is installed
+# Verifica e instala o netstat se necessário
 if ! command -v netstat &> /dev/null
 then
     echo "Netstat not found. Installing..."
@@ -66,28 +69,27 @@ else
     echo "Netstat is already installed."
 fi
 
-# Function to check if a port is available
+# Função para verificar se uma porta está disponível
 is_port_free() {
-    # Check if the port is not in use
     ! netstat -lnt | awk '$6 == "LISTEN" && $4 ~ ".*:'$1'$"' | grep -q "$1"
 }
 
-# Set the initial port
+# Define a porta inicial
 PORT=8069
 
-# Check if the port is available, if not, try the next one
+# Verifica se a porta está disponível, se não, tenta a próxima
 while ! is_port_free $PORT; do
     echo "Port $PORT is in use, trying the next one..."
     PORT=$((PORT + 1))
 done
 
-# Start the Odoo container
-echo "Starting the Odoo container on port $PORT..."
+# Inicia o container Odoo
+echo "Starting the Odoo container named ${PROJECT_NAME}_odoo on port $PORT..."
 docker run -d \
     -p $PORT:8069 \
     -v ${ADDITIONAL_MODULES_PATH}:/mnt/extra-addons \
-    --name meu_odoo \
-    --link meu_db:db \
+    --name ${PROJECT_NAME}_odoo \
+    --link ${PROJECT_NAME}_db:db \
     odoo:16.0
 
 echo "Done! Odoo should be available at http://localhost:$PORT"
